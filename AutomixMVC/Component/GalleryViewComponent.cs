1541f65e1d80c9adfdd64615d6f6aa3067e4d4aa
Z3roCo0l <104153656+Z3roCo0l@users.noreply.github.com>
2023-05-11 13:35:36 +0200

    Adding GalleryComponenet

---
+++ AutomixMVC/Component/GalleryViewComponent.cs
@@ -0,0 +1,50 @@
+﻿using AutomixMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AutomixMVC.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutomixMVC.Services;
+
+namespace AutomixMVC.Component
+{
+    public class GalleryViewComponent : ViewComponent
+    {
+        private readonly AutomixDbContext _context;
+        private readonly ILogger<GalleryViewComponent> _logger;
+        private readonly IImageService _imageService;
+
+        public GalleryViewComponent(AutomixDbContext context, ILogger<GalleryViewComponent> logger, IImageService imageService)
+        {
+            _context = context;
+            _logger = logger;
+            _imageService = imageService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var images = new List<Image>();
+
+            try
+            {
+                images = await _context.Images.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception.
+                _logger.LogError(ex, "An error occurred while getting images.");
+            }
+
+            // Create a ViewModel for your view that includes the Image and its Uri
+            var model = images.Select(image => new ImageViewModel
+            {
+                Image = image,
+                Uri = _imageService.GetUrl(image.Name)
+            }).ToList();
+
+            return View(model);
+        }
+    }
+}
--- AutomixMVC/Data/AutomixDbContext.cs
+++ AutomixMVC/Data/AutomixDbContext.cs
@@ -2,16 +2,41 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using AutomixMVC.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using Pomelo.EntityFrameworkCore.MySql;
+using System.Linq;
+
 
 namespace AutomixMVC.Data
 {
-    public class AutomixDbContext : IdentityDbContext
+    public class AutomixDbContext : DbContext
     {
-        public AutomixDbContext(DbContextOptions<AutomixDbContext> options) : base(options) { 
+        private readonly IConfiguration _configuration;
 
+        public AutomixDbContext(DbContextOptions<AutomixDbContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration;
         }
 
         public DbSet<Food> FoodItems { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                if (connectionString is null)
+                {
+                    throw new InvalidOperationException("The connection string was not found.");
+                }
+                optionsBuilder.UseMySQL(connectionString);
+            }
+        }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Food>()
@@ -27,4 +52,5 @@ namespace AutomixMVC.Data
             base.OnModelCreating(modelBuilder);
         }
     }
+
 }
